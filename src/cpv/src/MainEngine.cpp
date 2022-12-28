7a2baf1d5509c47fd2d73b16a9cc130d8768fe40
Andrej Susnik <susnik.andrej7@gmail.com>
2022-12-28 08:06:40 +0100

    Fix intellisense on linux

--- src/cpv/src/MainEngine.cpp
+++ src/cpv/src/MainEngine.cpp
@@ -21,6 +21,23 @@ namespace cpv
             std::cout << "Error parsing JSON" << std::endl;
             return;
         }
+
+        if(document.HasMember("vehicleEngine")) {
+            const rapidjson::Value& vehicleEngine = document["vehicleEngine"];
+            if(vehicleEngine.HasMember("torqueCurve")) {
+                const rapidjson::Value& torqueCurve = vehicleEngine["torqueCurve"];
+                if(torqueCurve.HasMember("x") && torqueCurve.HasMember("y")) {
+                    const rapidjson::Value& x = torqueCurve["x"];
+                    const rapidjson::Value& y = torqueCurve["y"];
+                    FunctionCurve3 engineTorqueCurve;
+                    if(x.IsArray() && y.IsArray() && x.Size() == y.Size()) {
+                        for (rapidjson::SizeType i = 0; i < x.Size(); i++) {
+                        }
+                        engine.setTorqueCurve(engineTorqueCurve);
+                    }
+                }
+            }
+        }
         
 
     }
